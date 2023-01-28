90cfb081bf364fb8020c2529ac410b186334022f
DarkVikos <darkvikos@gmail.com>
2023-01-28 11:20:07 +0100

    Init v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task-planer-ui';
+  username: string;
+
+  constructor() {
+    this.username = "User";
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskViewComponent } from './components/task-view/task-view.component';
+
+
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTreeModule} from '@angular/material/tree';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { MatCheckboxModule} from "@angular/material/checkbox";
+import { MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent,
+    TaskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
